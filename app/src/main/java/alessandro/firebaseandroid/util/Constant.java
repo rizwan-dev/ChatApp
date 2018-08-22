69f5b6c55ec742b4df0774e8fdef0882e17a5058
Rizwanul Haque <riztech.dev@gmail.com>
2018-08-23 00:23:05 +0530

    UI fixes

--- app/src/main/java/alessandro/firebaseandroid/util/Constant.java
+++ app/src/main/java/alessandro/firebaseandroid/util/Constant.java
@@ -6,5 +6,10 @@ public class Constant {
     public static final String PROFILE_PIC_LINK = "https://source.unsplash.com/random/3";
     public static final String UID = "TQYdgyFIBYY8TjuPgIQCuefKo6m1";
 
+   /* public static final String NAME = "Aniket";
+    public static final String EMAIL_ID = "g@test.com";
+    public static final String PROFILE_PIC_LINK = "https://firebasestorage.googleapis.com/v0/b/build-test-6d1db.appspot.com/o/usersProfilePics%2FJetpack_logo.png?alt=media&token=6e64ae66-2acb-46b7-adf0-e71ccdfac6d8";
+    public static final String UID = "yqlnAvTfmBVz8aqcUSf3S7k6UJD2";*/
+
     public static final String CHAT_DATA = "chat_data";
 }
